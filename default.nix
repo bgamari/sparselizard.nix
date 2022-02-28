638082911193c594f95b886c09ea096beb44a716
Ben Gamari <ben@smart-cactus.org>
2022-02-28 09:49:25 -0500

    Expose a set of packages

--- default.nix
+++ default.nix
@@ -2,20 +2,25 @@
 
 with pkgs;
 let
-  mpi = openmpi;
-  blas = openblasCompat;
+  things = rec {
+    inherit pkgs;
+    inherit (pkgs) gmsh paraview;
 
-  mumps = callPackage ./mumps { inherit blas; };
-  petsc = callPackage ./petsc {
-    inherit mumps sowing mpi blas;
-  };
-  slepc = callPackage ./slepc { inherit mpi petsc blas; };
-  sowing = callPackage ./sowing { };
-  scalapack = pkgs.scalapack.override { inherit mpi; };
+    mpi = openmpi;
+    blas = openblasCompat;
 
-  sparselizard = callPackage ./sparselizard {
-    inherit petsc blas slepc openmpi mumps;
+    mumps = callPackage ./mumps { inherit blas; };
+    petsc = callPackage ./petsc {
+      inherit mumps sowing mpi blas;
+    };
+    slepc = callPackage ./slepc { inherit mpi petsc blas; };
+    sowing = callPackage ./sowing { };
+    scalapack = pkgs.scalapack.override { inherit mpi; };
+
+    sparselizard = callPackage ./sparselizard {
+      inherit petsc blas slepc openmpi mumps;
+    };
   };
 
 in
-sparselizard
+things
